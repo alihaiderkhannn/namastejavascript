ed915d91bb558921b5bf463dced9e8c4e092533a
Ali Haider Khan <ali_786@live.no>
2025-04-28 13:33:44 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+
+/* var x = 7;
+
+function getName(){     
+    console.log("Namaste Javascript");
+ }
+
+getName(); this will print out "Namaste Javascript".
+console.log(x); this will print out 7. 
+
+JavaScript first hoists (prepares) variables and functions before running the code.
+
+var x = 7; means x is first set as undefined, then updated to 7.
+
+function getName() {...} is fully hoisted, so it can be called before or after its definition.
+
+When getName(); runs, it prints 👉 "Namaste Javascript".
+
+Then console.log(x); prints 👉 7, because x was assigned that value earlier. */
+
+
+getName(); // this will print out "Namaste Javascript".
+console.log(x);
+
+var x = 7;
+
+function getName(){
+    console.log("Namaste Javascript");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
